a10e45a323445dd11a12a05dd3ba34cf7c5dca3c
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-06-04 12:19:12 -0700

    Complete Knight Traversal Algorithm

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+import { Queue } from './Queue.js';
+
+class Path {
+  constructor(currentNode, path) {
+    this.currentNode = currentNode;
+    this.path = path;
+  }
+}
+
 function getValidMoves(current) {
-  let i = current[0];
-  let j = current[1];
+  let i = current.currentNode[0];
+  let j = current.currentNode[1];
   const moves = [
     [i + 2, j + 1],
     [i + 2, j - 1],
@@ -11,7 +20,7 @@ function getValidMoves(current) {
     [i - 1, j + 2],
     [i - 1, j - 2],
   ];
-  const possibleMoves = [];
+  let possibleMoves = [];
   for (let i = 0; i < moves.length; i++) {
     if (
       moves[i][0] >= 0 &&
@@ -26,7 +35,42 @@ function getValidMoves(current) {
 }
 
 function knightMoves(start, target) {
-  console.log(getValidMoves(start));
+  const visitedNodes = new Set();
+  const queue = new Queue();
+  const startPath = new Path(start, [start]);
+  visitedNodes.add(`${start[0]},${start[1]}`);
+  queue.enqueue(startPath);
+  while (!queue.isEmpty()) {
+    let currentPosition = queue.peek();
+    queue.dequeue();
+    //check if we have reached target
+    if (
+      currentPosition.currentNode[0] === target[0] &&
+      currentPosition.currentNode[1] === target[1]
+    ) {
+      console.log(
+        `You made it in ${currentPosition.path.length - 1} moves! Heres your path!`,
+      );
+      currentPosition.path.forEach((element) => {
+        console.log(`[${element}]`);
+      });
+      break;
+    }
+
+    //check valid moves and create obj
+    let validMoves = getValidMoves(currentPosition);
+    for (let i = 0; i < validMoves.length; i++) {
+      //update path
+      if (!visitedNodes.has(`${validMoves[i][0]},${validMoves[i][1]}`)) {
+        let newPath = new Path(validMoves[i], [
+          ...currentPosition.path,
+          validMoves[i],
+        ]);
+        queue.enqueue(newPath);
+        visitedNodes.add(`${validMoves[i][0]},${validMoves[i][1]}`);
+      }
+    }
+  }
 }
 
-knightMoves([2, 2], [1, 2]);
+knightMoves([3, 3], [4, 3]);
